22d44beae5b133be79fe285ed9ab7478ed254bc2
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2024-04-21 22:40:03 -0300

    feat: change password schema

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -5,7 +5,7 @@ export const users = pgTable('users', {
   id: text('id').$defaultFn(() => createId()).primaryKey(),
   name: text('name').notNull(),
   email: text("email").notNull().unique(),
-  password: text('password_hash'),
+  password: text('password_hash').notNull(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
 });
